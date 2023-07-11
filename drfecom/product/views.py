af352faa1aa3a516551d1b5ec091be57ebe1d2fa
Md Shayon <mdshayon0@gmail.com>
2023-07-11 19:58:04 +0600

    T16 - DRF viewsets

--- drfecom/product/views.py
+++ drfecom/product/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Category
+from .serializers import CategotySerielizer
 
 # Create your views here.
+class CategoryView(viewsets.ViewSet):
+    """
+    TODO: A simple viewset for viewing categories -> https://www.django-rest-framework.org/api-guide/viewsets/
+    """
+    questyset = Category.objects.all()
+
+    def list(self, request):
+        serielizer = CategotySerielizer(self.questyset, many=True)
+        return Response(serielizer.data)
+
