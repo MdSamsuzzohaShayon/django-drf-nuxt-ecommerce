cee2344e4ffcae7c1984b3760a486ace7413dcc5
Md Shayon <mdshayon0@gmail.com>
2023-07-18 20:26:46 +0600

    croud operations is been done for product and category

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Furniture ecommerce website
 
- - [Tutorial](https://www.youtube.com/watch?v=jUsm_LV4_cE&list=PLOLrQ9Pn6cawinBJbH5d9IfloO9RRPMiq&index=8), [Tutorial 2](https://www.youtube.com/watch?v=EbLEyM9SyZQ&list=PLOLrQ9Pn6cay_cQkyg-WYYiJ_EKU8KWKh), [tutorial 3](https://www.youtube.com/watch?v=tujhGdn1EMI)
+ - [Tutorial](https://www.youtube.com/watch?v=jUsm_LV4_cE&list=PLOLrQ9Pn6cawinBJbH5d9IfloO9RRPMiq&index=8), [Tutorial 2](https://www.youtube.com/watch?v=EbLEyM9SyZQ&list=PLOLrQ9Pn6cay_cQkyg-WYYiJ_EKU8KWKh), [tutorial 3](https://www.youtube.com/watch?v=tujhGdn1EMI), [Tutorial 4](https://www.youtube.com/watch?v=xjMP0hspNLE&list=PL-51WBLyFTg1gPEHotYAhNAPsisChkyTc), [tutorial 5](https://www.youtube.com/watch?v=lo7lBD9ynVc&t=14s)
+
+### Instructions
+ - On delete product or category, images should be deleted from the folder too
+ - Search by product and category
+ - Filter products by different value
 
  - Setup
  ```
@@ -43,7 +48,6 @@
  - https://99grid.com/
  - django-rest-framework-json-api - https://github.com/django-json-api/django-rest-framework-json-api
  - Django REST Registration - https://github.com/apragacz/django-rest-registration
- - deep-dive-drf-model-serializer-relations - https://github.com/oscarychen/deep-dive-drf-model-serializer-relations
  - ecommerce_api - https://github.com/thomas545/ecommerce_api
 
  - Left at __https://www.youtube.com/watch?v=ktuOUaOyMmo&list=PLOLrQ9Pn6cawinBJbH5d9IfloO9RRPMiq&index=31__
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,15 +1,61 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Product, Category
-from .serializers import ProductSerializer, CategorySerializer
+from .serializers import ProductSerializer, CategorySerializer, ProductUpdateSerializer, CategoryUpdateSerializer
 
-# Create your views here.
+# Products
 class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 class ProductCreateView(generics.CreateAPIView):
     serializer_class = ProductSerializer
+    
+    # def perform_create(self, serializer):
+    #     instance = serializer.save()
+    #     print("Category create instance(perform_create)->" + instance)
 
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductUpdateView(generics.UpdateAPIView):
+    serializer_class = ProductUpdateSerializer
+    queryset = Product.objects.all()
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        print(instance)
+        if instance.title:
+            del instance.title
+
+class ProductDeleteView(generics.DestroyAPIView):
+    serializer_class = ProductSerializer
+
+
+# Categories
 class CategoryCreateView(generics.CreateAPIView):
     serializer_class = CategorySerializer
+
+    # def perform_create(self, serializer):
+    #     name = serializer.validated_data.get('name')
+    #     # instance = serializer.save(name=name)
+    #     print("Category create instance(perform_create)->" + name)
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryUpdateView(generics.UpdateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategoryUpdateSerializer
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        print(instance)
+
+class CategoryDeleteView(generics.DestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = "pk"
+        
