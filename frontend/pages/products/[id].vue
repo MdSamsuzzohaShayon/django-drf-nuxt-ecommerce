9aef810ab6f3d970f624285905d6f971c79e3b04
Md Shayon <mdshayon0@gmail.com>
2023-07-25 04:37:10 +0600

    adding path parameters

---
+++ frontend/pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>Product detail {{ id }}</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum culpa cupiditate fugit nisi alias officiis eaque aliquid, minus architecto consectetur.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum culpa cupiditate fugit nisi alias officiis eaque aliquid, minus architecto consectetur.</p>
+    </div>
+</template>
+
+<script setup>
+const {id} = useRoute().params
+
+</script>
+
+<style scoped>
+
+</style>
