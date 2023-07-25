713030fe01c6c4ac5f954e74313e012db9338b54
Md Shayon <mdshayon0@gmail.com>
2023-07-26 01:03:44 +0600

    error pages

--- frontend/pages/products/[id].vue
+++ frontend/pages/products/[id].vue
@@ -9,6 +9,10 @@ const { id } = useRoute().params;
 const uri = "http://localhost:8000/api/products/" + id + "/";
 const {data: product} = await useFetch(uri, {key: id});
 
+if (!product.value){
+    throw createError({statusCode: 404, statusMessage: "Product not found", fetal: true});
+}
+
 definePageMeta({
     layout: 'products'
 });
